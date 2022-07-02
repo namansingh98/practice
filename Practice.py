62347029ec839443cd043868b7728da4d2e8f737
Naman Rathore <108014558+namansingh98@users.noreply.github.com>
2022-07-02 23:37:28 +0530

    just do it

---
+++ Practice.py
@@ -0,0 +1,34 @@
+#list are mutable  we can add update
+# order indexing and slcing are allowed 
+#heterogeneous data type
+
+from operator import index
+
+
+l = [1,2,3,4,5,"a","b","c","d", 10, 20, 30, 40,]
+# print(l,type(l))
+
+
+#indexing and sliciing
+print(l[-1])
+
+print(l[2:-5])
+
+
+# # # Operators in Python 1 Arithmetic operators +,-,*,/,//1%," 
+2 Comparison operators <I>,<=1>=,==,!= 
+num1 = 10 # num2 =20 # print(numl + num2) # print(numl - num2) # print(numl * num2) # print(10/3) # print(10//3) # print(10%3) 
+print(45 ** 6) 
+
+# statements # elif [condition]: # statemnts # else: # statements 
+num1 = 100 
+num2 = 500 
+# 1 numi is greater # 2 num2 is greater # 3 numi and num2 are equal 
+if num1 > num2: 
+    print("num1 is greater than num2") 
+elif num2 >num1: 
+    print("num2 is greater than num1") 
+else: 
+    print("num1 and Num2 are equal") 
+
+
